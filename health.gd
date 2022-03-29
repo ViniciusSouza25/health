13858d03ce8344d988d7add602b472fc414755cd
ViniciusSouza25 <99210113+ViniciusSouza25@users.noreply.github.com>
2022-03-29 14:16:58 -0300

    versao nova
    muito foda

--- health.gd
+++ health.gd
@@ -6,6 +6,28 @@ onready var player2 = $bot2
 #variavel 2 para o player 2
 onready var player3 = $bot3
 onready var player4 = $bot4
+
+
+
+onready var resposta = $Button
+onready var resposta2 = $Button2
+onready var resposta3 = $button3
+
+
+
+onready var rect = $TextureRect3
+onready var rect2 = $TextureRect4
+onready var rect3 = $TextureRect5
+
+
+onready var label_res = $resposta1
+onready var label_res2 = $resposta2
+onready var label_res3 = $resposta3
+
+
+
+
+
 var  vida  = 100
 var vida2 = 100
 var dano =  30
@@ -13,7 +35,7 @@ func _ready():
 	get_node("ProgressBar").value = vida
 	get_node("ProgressBar2").value = vida2
 	$Label3.text = String(Global.perguntas["pergunta 1"]["pergunta"]["voto impresso"][0])
-	#texto.text = String(Global.dialogos_dados["Introdução"][textoatual])
+	
 	$resposta1.text = String(Global.perguntas["pergunta 1"]["respostas"][0])
 	$resposta2.text = String(Global.perguntas["pergunta 1"]["respostas"][1])
 	$resposta3.text = String(Global.perguntas["pergunta 1"]["respostas"][2])
@@ -24,15 +46,15 @@ func _ready():
 func _on_Button_pressed():
 	if self.has_node("bot1"):
 		if dano > 0 :
-			vida = (vida - dano)
-		if vida <= 0: 
-			dano = 0
-			player.queue_free()
-		#print("dano : ",dano)
-		#print("vida : ",vida)
-		get_node("ProgressBar").value = vida
-		#$Label3.text = String("pergunta z")
-		
+			rect.show()
+			resposta.hide()
+			resposta2.hide()
+			resposta3.hide()
+			label_res.hide()
+			label_res2.hide()
+			label_res3.hide()
+			
+			
 
 
 
@@ -46,28 +68,14 @@ func _on_Button_pressed():
 
 func _on_Button2_pressed():
 	if self.has_node("bot2"):
-			if dano > 0 :
-				vida2 = (vida2 - dano)
-			if vida2 <= 0 :
-				dano = 0
-				player2.queue_free()
-			get_node("ProgressBar2").value = vida2
-			print("vida =", vida2)
-			#$Label3.text = String("pergunta y")
-		
-	if self.has_node("bot3"):
-			#if dano > 0 :
-			#	vida2 = (vida2 - dano)
-			if vida2 <= 0 :
-				dano = 0
-				player3.queue_free()
-			
-	if self.has_node("bot4"):
-			#if dano > 0 :
-				#vida2 = (vida2 - dano)
-			if vida2 <= 0 :
-				dano = 0
-				player4.queue_free()
+			if dano > 0:
+				rect2.show()
+				resposta.hide()
+			resposta2.hide()
+			resposta3.hide()
+			label_res.hide()
+			label_res2.hide()
+			label_res3.hide()
 		
 
 
@@ -75,12 +83,108 @@ func _on_Button2_pressed():
 
 func _on_button3_pressed():
 	if self.has_node("bot1"):
-		if dano > 0 :
-			vida = (vida - dano)
+		rect3.show()
+		resposta.hide()
+		resposta2.hide()
+		resposta3.hide()
+		label_res.hide()
+		label_res2.hide()
+		label_res3.hide()
+	
+
+
+func _on_botao_expancao_pressed():
+	if vida > 0:
+		vida = (vida - dano)
+		rect.hide()
+		resposta.show()
+		resposta2.show()
+		resposta3.show()
+		label_res.show()
+		label_res2.show()
+		label_res3.show()
 		if vida <= 0: 
 			dano = 0
 			player.queue_free()
-		#print("dano : ",dano)
-		#print("vida : ",vida)
+			
+		
+		
 		get_node("ProgressBar").value = vida
-	#	$Label3.text = String("pergunta x")
+		
+
+
+func _on_botao_expancao2_pressed():
+	rect.hide()
+	resposta.show()
+	resposta2.show()
+	resposta3.show()
+	label_res.show()
+	label_res2.show()
+	label_res3.show()
+
+
+func _on_botao2_expancao_pressed():
+	if dano > 0 :
+			vida2 = (vida2 - dano)
+			rect2.hide()
+			resposta.show()
+			resposta2.show()
+			resposta3.show()
+			label_res.show()
+			label_res2.show()
+			label_res3.show()
+			if vida2 <= 0 :
+				dano = 0
+				player2.queue_free()
+			get_node("ProgressBar2").value = vida2
+			print("vida =", vida2)
+			
+		
+	if self.has_node("bot3"):
+			
+			if vida2 <= 0 :
+				dano = 0
+				player3.queue_free()
+			
+	if self.has_node("bot4"):
+			
+			if vida2 <= 0 :
+				dano = 0
+				player4.queue_free()
+
+
+func _on_botao2_expancao2_pressed():
+	rect2.hide()
+	resposta.show()
+	resposta2.show()
+	resposta3.show()
+	label_res.show()
+	label_res2.show()
+	label_res3.show()
+
+
+func _on_botao3_expancao_pressed():
+	if dano > 0 :
+		vida = (vida - dano)
+		rect3.hide()
+		resposta.show()
+		resposta2.show()
+		resposta3.show()
+		label_res.show()
+		label_res2.show()
+		label_res3.show()
+		if vida <= 0: 
+			dano = 0
+			player.queue_free()
+		
+		get_node("ProgressBar").value = vida
+
+
+func _on_botao3_expancao2_pressed():
+	rect3.hide()
+	resposta.show()
+	resposta2.show()
+	resposta3.show()
+	label_res.show()
+	label_res2.show()
+	label_res3.show()
