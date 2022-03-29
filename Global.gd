da22e0d7dd89c5a9580c5b12b278a55bda88ec8f
ViniciusSouza25 <99210113+ViniciusSouza25@users.noreply.github.com>
2022-03-29 11:27:06 -0300

    foda
    mandei

--- Global.gd
+++ Global.gd
@@ -1,16 +1,17 @@
 extends Node
 
+var perguntas = {
+	"pergunta 1": {
+		"pergunta": {
+			"voto impresso" :{
+				0: "voto impresso"
+			}
+		}  ,
+		"respostas": {
+			0: "resposta1",
+			1: "resposta2",
+			2: "resposta3"
+		}
+	}
+}
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- health.gd
+++ health.gd
@@ -4,13 +4,19 @@ onready var player = $bot1
 #variavel 1 para o player 1
 onready var player2 = $bot2
 #variavel 2 para o player 2
+onready var player3 = $bot3
+onready var player4 = $bot4
 var  vida  = 100
 var vida2 = 100
-var dano =  10
+var dano =  30
 func _ready():
 	get_node("ProgressBar").value = vida
 	get_node("ProgressBar2").value = vida2
-	$Label3.text = String("pergunta 1")
+	$Label3.text = String(Global.perguntas["pergunta 1"]["pergunta"]["voto impresso"][0])
+	#texto.text = String(Global.dialogos_dados["Introdução"][textoatual])
+	$resposta1.text = String(Global.perguntas["pergunta 1"]["respostas"][0])
+	$resposta2.text = String(Global.perguntas["pergunta 1"]["respostas"][1])
+	$resposta3.text = String(Global.perguntas["pergunta 1"]["respostas"][2])
 	
 
 
@@ -25,7 +31,8 @@ func _on_Button_pressed():
 		#print("dano : ",dano)
 		#print("vida : ",vida)
 		get_node("ProgressBar").value = vida
-		$Label3.text = String("pergunta z")
+		#$Label3.text = String("pergunta z")
+		
 
 
 
@@ -46,12 +53,27 @@ func _on_Button2_pressed():
 				player2.queue_free()
 			get_node("ProgressBar2").value = vida2
 			print("vida =", vida2)
-			$Label3.text = String("pergunta y")
+			#$Label3.text = String("pergunta y")
+		
+	if self.has_node("bot3"):
+			#if dano > 0 :
+			#	vida2 = (vida2 - dano)
+			if vida2 <= 0 :
+				dano = 0
+				player3.queue_free()
 			
+	if self.has_node("bot4"):
+			#if dano > 0 :
+				#vida2 = (vida2 - dano)
+			if vida2 <= 0 :
+				dano = 0
+				player4.queue_free()
 		
 
 
-func _on_Button3_pressed():
+
+
+func _on_button3_pressed():
 	if self.has_node("bot1"):
 		if dano > 0 :
 			vida = (vida - dano)
@@ -61,15 +83,4 @@ func _on_Button3_pressed():
 		#print("dano : ",dano)
 		#print("vida : ",vida)
 		get_node("ProgressBar").value = vida
-		$Label3.text = String("pergunta x")
-
-func _process (delta):
-	get_node("ProgressBar2").value = vida2
-	$Label4.text = String(vida2)
-	if vida2 < 100:
-		$Label4.show()
-		$Label5.show() 
-		$Label6.hide()
-	else:
-		$Label6.show()
-	
+	#	$Label3.text = String("pergunta x")
